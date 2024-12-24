519c084807d7b4408745725482c02de70ce72bb4
MxAziz <hereabdulaziz@gmail.com>
2024-12-24 19:33:02 +0600

    successfully added get request for car details page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -51,6 +51,13 @@ async function run() {
       const cars = await carsCollection.find().toArray();
       res.send(cars);
     });
+
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carsCollection.findOne(query);
+      res.send(result);
+    })
 
 
   } finally {
