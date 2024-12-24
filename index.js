8c44719d808094d36f36966ac6475788d762edbd
MxAziz <hereabdulaziz@gmail.com>
2024-12-24 11:48:56 +0600

    added get request for available cars page

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -16,7 +13,6 @@ app.use(
   })
 );
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hathz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,18 +25,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-// verify the user token with custom middleware.
-const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).send({ message: "Unauthorized access" });
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).send({ message: "Forbidden access" });
-    req.user = decoded;
-    next();
-  });
-};
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -49,33 +33,24 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
-      );
+    );
 
-      const carsCollection = client.db("carsDB").collection("cars");
+    const carsCollection = client.db("carsDB").collection("cars");
 
-      //   auth related api
-      app.post('jwt', async (req, res) => {
-          const user = req.body;
-          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
-          res
-              .cookie('token', token, {
-                  httpOnly: true,
-                  secure: false,
-              })
-              .send({ success: true });
-      })
+    // All cars collection.
+    app.post("/cars", async (req, res) => {
+      const car = req.body;
+      car.bookingStatus = "Available";
+      car.dateAdded = new Date();
 
-    //   all cars collection.
-       app.post("/cars",verifyToken,  async (req, res) => {
-         const car = req.body;
-         car.bookingStatus = "Available";
-         car.user = req.user.email;
-         car.dateAdded = new Date();
+      const result = await carsCollection.insertOne(car);
+      res.send(result);
+    });
 
-         const result = await carsCollection.insertOne(car);
-         res.send(result);
-       });
-
+    app.get("/cars", async (req, res) => {
+      const cars = await carsCollection.find().toArray();
+      res.send(cars);
+    });
 
 
   } finally {
