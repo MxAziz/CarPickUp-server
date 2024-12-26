94ab32f4e4d44cb259dceba05df4e0d6d4af7e04
MxAziz <hereabdulaziz@gmail.com>
2024-12-26 23:48:58 +0600

    deploy server side

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hathz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
